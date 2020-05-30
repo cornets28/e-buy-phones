641be06da3c972c902f7d0c26469c0a8e3fb764a
CornetS28 <corsam28@gmail.com>
2020-05-30 16:44:30 -0400

    added font awesome link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { render } from '@testing-library/react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">coloumn 1</div>
+          <div className="col-6">coloumn 2</div>
+        </div>
+      </div>
+
+    )
+   
+  }
 }
 
 export default App;

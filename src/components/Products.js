63d2e2b80049f8c3c070b63b4cca15dda0bf7e36
CornetS28 <corsam28@gmail.com>
2020-06-04 18:11:26 -0400

    import SingleProduct.js  in Products.js file

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Product from './SingleProduct';
 
 export default class Products extends Component {
   render() {
     return (
       <div>
         <h3 className="">All Products</h3>
+        <Product />
       </div>
     )
   }

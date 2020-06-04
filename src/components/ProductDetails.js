94a7ac3bbedb2b72851a06c09601c8b3e9cb817b
CornetS28 <corsam28@gmail.com>
2020-06-04 17:52:01 -0400

    add ProductDetails.js file

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="">Prouct details</h3>
+      </div>
+    )
+  }
+}

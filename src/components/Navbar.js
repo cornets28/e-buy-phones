a4023edacf8e486b11e01a248d5b3bd77050d7a0
CornetS28 <corsam28@gmail.com>
2020-06-04 17:36:33 -0400

    add Navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="">You are now in the navbar component</h3>
+      </div>
+    )
+  }
+}

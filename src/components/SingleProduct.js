3565279a0c815b82333631a9f3e063e3f09c2c27
CornetS28 <corsam28@gmail.com>
2020-06-04 17:47:59 -0400

    add SingleProduct.js file

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleProduct extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="">Single Product</h3>
+      </div>
+    )
+  }
+}

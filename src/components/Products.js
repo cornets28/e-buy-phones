d2aa5dd2eb30e0235905532d1416618828b75112
CornetS28 <corsam28@gmail.com>
2020-06-04 18:42:18 -0400

    fix minor error in Product.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import Product from './SingleProduct';
+import SingleProduct from './SingleProduct';
 
 export default class Products extends Component {
   render() {
     return (
       <div>
         <h3 className="">All Products</h3>
-        <Product />
+        <SingleProduct />
       </div>
-    )
+    );
   }
 }

db58a9a21276a10f3f84cc70688573406b969665
CornetS28 <corsam28@gmail.com>
2020-06-06 18:41:58 -0400

    add cart button in the navabar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logoApp from '../logo.svg';
+import styled from "styled-components";
+
+import {BtnContainer} from "./Button";
+import logoApp from "../logo.svg";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-sm  bg-danger navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
         Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk */}
@@ -13,7 +16,24 @@ export default class Navbar extends Component {
         <Link to="/">
           <img src={logoApp} alt="logo" className="navbar-brand" />
         </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Phones
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          < BtnContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my cart
+          </ BtnContainer>
+        </Link>
       </nav>
     );
   }
 }
+
+

96b443575496ca3f1e3adda95fb45d41ea781664
CornetS28 <corsam28@gmail.com>
2020-06-06 19:37:43 -0400

    create Title component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
-import SingleProduct from './SingleProduct';
+// import SingleProduct from './SingleProduct';
+import Title from './Title';
 
 export default class Products extends Component {
+ 
+  state = {
+     products: []
+   };
+
   render() {
     return (
-      <div>
-        <h3 className="">All Products</h3>
-        <SingleProduct />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="phones" />
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }

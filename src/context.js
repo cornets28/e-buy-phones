b280c0d24c1fed71fa272d988544861952d0995b
CornetS28 <corsam28@gmail.com>
2020-06-06 21:18:00 -0400

    create detailHandler and addToCart methods

--- src/context.js
+++ src/context.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ourPhones, detailPhone } from "./data";
 
 const PhoneContext = React.createContext();
-// CONTEXT A=HAS 2 STEPS: 1. Provider AND 2.Consumer 
+// CONTEXT A=HAS 2 STEPS: 1. Provider AND 2.Consumer
 
 class PhoneProvider extends Component {
+  state = {
+    phones: ourPhones,
+    detailPhone,
+  };
+  detailHandler() {
+    console.log("hello from detail handler");
+  }
+
+  addToCart() {
+    console.log("hello from add to cart");
+  }
   render() {
     return (
-      <PhoneContext.Provider value="this is from context">
+      <PhoneContext.Provider
+        value={{
+           ...this.state,
+          detailHandler: this.detailHandler,
+          addToCart: this.addToCart,
+        }}
+      >
         {this.props.children}
       </PhoneContext.Provider>
-    )
+    );
   }
 }
 const PhoneConsumer = PhoneContext.Consumer;

d97c4d03608ef8bcc2234258acf5684f5a5d3e4d
CornetS28 <corsam28@gmail.com>
2020-06-16 14:48:22 -0400

    modify button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const BtnContainer = styled.button`
-         text-transform: capitalize;
-         background: transparent;
-         font-size: 1.4rem;
-         border: 0.06rem solid var(--defaultLightYellow);
-         color: ${(props) =>
-           props.cart
-             ? "var(--defaultLightYellow)"
-             : "var(--defaultLightPink)"};
-         padding: 0.2rem 0.5rem;
-         margin: 0.2rem 0.5rem 0.2rem 0;
-         border-radius: 0.5rem;
-         border-color: ${(props) =>
-           props.cart
-             ? "var(--defaultLightYellow)"
-             : "var(--defaultLightPink)"};
-         cursor: pointer;
-         transition: all 0.5s ease-in-out;
-         &:hover {
-           background: ${(props) =>
-             props.cart
-               ? "var(--defaultLightPink)"
-               : "var(--defaultLightYellow)"};
-         }
-         &:focus {
-           outline: none;
-         }
-       `;
+  text-transform: capitalize;
+  background: transparent;
+  font-size: 1.4rem;
+  border: 0.06rem solid var(--defaultLightYellow);
+
+  color: ${(props) =>
+    props.cart ? "var(--defaulPink)" : "var(--defaultPink)"};
+  padding: 0.2rem 0.5rem;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  border-radius: 0.5rem;
+  color: var(--defaultLightYellow);
+  border-color: ${(props) =>
+    props.cart ? "var(--defaulPink)" : "var(--defaultLightYellow)"};
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    /* background: var(--defaultLightYellow); */
+    background: ${(props) =>
+      props.cart ? "var(--defaultLightPink)" : "var(--defaultLightYellow)"};
+    color: var(--defaulPink);
+  }
+  &:focus {
+    outline: none;
+  }
+`;

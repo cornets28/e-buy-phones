169cc87f2d27d5c95f427120f2dfc3e0b92b8b8e
CornetS28 <corsam28@gmail.com>
2020-06-19 17:56:52 -0400

    add  openModal and closeMoadal methods

--- src/context.js
+++ src/context.js
@@ -7,39 +7,79 @@ const PhoneContext = React.createContext();
 class PhoneProvider extends Component {
   state = {
     phones: [],
-    detailPhone
+    detailPhone: detailPhone,
+    cart: [],
+    modalIsOpen: false,
+    modalPhone: detailPhone,
   };
- // Get the copy of the data, instead of referencing (the original) them
- // also called 'DESTRUCTORING'
- componentDidMount(){
-   this.setPhones();
- }
+  // Get the copy of the data, instead of referencing (the original) them
+  // also called 'DESTRUCTORING'
+  componentDidMount() {
+    this.setPhones();
+  }
   setPhones = () => {
     let tempPhones = [];
-    ourPhones.forEach( phone => {
+    ourPhones.forEach((phone) => {
       const singlePhone = { ...phone };
       tempPhones = [...tempPhones, singlePhone];
     });
     this.setState({
-      phones: tempPhones
-    })
-  }
+      phones: tempPhones,
+    });
+  };
   // Get the copy of the data (ENDS HERE)
 
-  detailHandler() {
-    console.log("hello from detail handler");
-  }
+  getPhoneId = (id) => {
+    return this.state.phones.find((phone) => phone.id === id);
+  };
 
-  addToCart(id) {
-    console.log(`This is the id: ${id}`);
-  }
+  detailHandler = (id) => {
+    const phone = this.getPhoneId(id);
+    this.setState(() => {
+      return { detailPhone: phone };
+    });
+  };
+
+  addToCart = (id) => {
+    let tempPhones = [...this.state.phones];
+    const index = tempPhones.indexOf(this.getPhoneId(id));
+    const phone = tempPhones[index];
+    phone.inCart = true;
+    phone.count = 1;
+    const price = phone.price;
+    phone.total = price;
+
+    this.setState(() => {
+      // return {
+      // phones: [...tempPhones],
+      // cart: [...this.state.cart, phone],
+      // detailphone: { ...phone },
+      // };
+    });
+  };
+
+  openModal = (id) => {
+    const phone = this.getPhoneId(id);
+    this.setState(() => {
+      return { modalPhone: phone, modalIsOpen: true };
+    });
+  };
+
+  closeModal = () => {
+    this.setState(() => {
+      return { modalIsOpen: false };
+    });
+  };
+
   render() {
     return (
       <PhoneContext.Provider
         value={{
-           ...this.state,
+          ...this.state,
           detailHandler: this.detailHandler,
           addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
         }}
       >
         {this.props.children}

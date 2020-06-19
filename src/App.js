634954ef8c3884a132aacd5c7594d802983b9bc1
CornetS28 <corsam28@gmail.com>
2020-06-19 18:49:35 -0400

    set modal is open to false

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from "./components/Navbar";
@@ -9,6 +8,7 @@ import SingleProduct from "./components/SingleProduct";
 import ProductDetails from "./components/ProductDetails";
 import Cart from "./components/Cart";
 import PageNotFound from "./components/PageNotFound";
+import Modal from './components/Modal'
 
 class App extends Component {
   render() {
@@ -22,6 +22,7 @@ class App extends Component {
           <Route path="/cart" component={Cart} />
           <Route component={PageNotFound} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
    
